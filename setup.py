dbc13251cb7afb99587eca2af601359977d75c2e
Charles-Henri de Boysson <ceache@users.noreply.github.com>
2018-04-30 10:10:14 -0400

    Build update: remove setup requires, cleanup tox config, prune old code.

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-"""Treadmill setup.py."""
+"""Treadmill AWS setup.py.
+"""
+
+import setuptools
 
 # pip 10.0 moved req to _internal. Need to find better solution, changing
 # for now so that build pass.
@@ -13,18 +16,13 @@ def _read_requires(filename):
     reqs = []
     for inst_req in pip_req.parse_requirements(filename, session='no session'):
         req = str(inst_req.req)
-        if not inst_req.match_markers():
-            print('Skipping %r: %r => False' % (req, inst_req.markers))
-            continue
-        reqs.append(str(inst_req.req))
+        if inst_req.markers:
+            req += '; %s' % inst_req.markers
+        reqs.append(req)
     return reqs
 
 
-from setuptools import setup  # pylint: disable=wrong-import-position
-
-
-setup(
-    version='3.7',
+setuptools.setup(
+    version='1.0',
     install_requires=_read_requires('requirements.txt'),
-    setup_requires=_read_requires('test-requirements.txt')
 )
