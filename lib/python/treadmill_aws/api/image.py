1bf1e4eac9880bebe58f9018e1e8a62d3aca77cd
Andrei Keis <andreikeis@users.noreply.github.com>
2018-09-19 14:57:14 -0400

    Added initial implementation of image REST API.

--- lib/python/treadmill_aws/api/image.py
+++ lib/python/treadmill_aws/api/image.py
@@ -1,49 +1,148 @@
 """Treadmill AWS image API.
 """
 
+import logging
+
 from treadmill import authz
+from treadmill import exc
 
 from treadmill_aws import ec2client
 from treadmill_aws import awscontext
+from treadmill_aws import userdata as ud
+from treadmill_aws.cli import admin as aws_cli_admin
 
 
+_LOGGER = logging.getLogger(__name__)
+
+
+# pylint: disable=too-many-statements
 class API:
     """AWS Image REST api."""
 
-    def __init__(self):
+    def __init__(self,
+                 account=None,
+                 instance_profile=None,
+                 secgroup=None,
+                 subnet=None):
+
+        self._account = str(account)
+        self._instance_profile = instance_profile
+        self._secgroup = secgroup
+        self._subnet = subnet
 
         def _list(account=None):
             """List configured monitors."""
+            _LOGGER.info('list: account=%s', account)
             ec2_conn = awscontext.GLOBAL.ec2
             if not account:
-                account = awscontext.GLOBAL.sts.get_caller_identity().get(
-                    'Account'
-                )
+                account = self._account
 
             image = {}
-            images = ec2client.list_images(ec2_conn, owners=[account], **image)
-            return images
+            return ec2client.list_images(ec2_conn, owners=[account], **image)
 
-        def get(_rsrc_id):
+        def get(rsrc_id, account=None):
             """Get AWS image configuration."""
-            return {}
+            _LOGGER.info('get: %s', rsrc_id)
+            ec2_conn = awscontext.GLOBAL.ec2
+            if not account:
+                account = self._account
 
-        def create(_rsrc_id, _rsrc):
+            image = {
+                'name': rsrc_id
+            }
+            return ec2client.get_image(
+                ec2_conn, owners=[account], **image
+            )
+
+        def create(rsrc_id, rsrc):
             """Create (configure) AWS image."""
+            image = rsrc_id
+            _LOGGER.info('Create image: %s', image)
+
+            ec2_conn = awscontext.GLOBAL.ec2
+            sts_conn = awscontext.GLOBAL.sts
+
+            # Check if image exists. Create is async, so this is optimization
+            # to return Found early.
+            found = False
+            try:
+                get(image)
+                found = True
+            except Exception as _err:  # pylint: disable=broad-except
+                # TODO: catch appropriate exception.
+                pass
+
+            if found:
+                raise exc.FoundError('Image {} already exists.'.format(image))
+
+            base_image_account = rsrc.get('base_image_account')
+            _LOGGER.info('base account: %s', base_image_account)
+
+            base_image = rsrc.get('base_image')
+            base_image_id = aws_cli_admin.image_id(
+                ec2_conn, sts_conn, base_image, account=base_image_account)
+            _LOGGER.info('base image id: %s', base_image_id)
+
+            secgroup = rsrc.get('secgroup')
+            if secgroup:
+                secgroup_id = aws_cli_admin.secgroup_id(ec2_conn, secgroup)
+            else:
+                secgroup_id = self._secgroup
+            _LOGGER.info('secgroup id: %s', secgroup_id)
+
+            subnet = rsrc.get('subnet')
+            if subnet:
+                subnet_id = aws_cli_admin.subnet_id(ec2_conn, subnet)
+            else:
+                subnet_id = self._subnet
+            _LOGGER.info('subnet id: %s', secgroup_id)
+
+            key = rsrc.get('key')
+
+            cloud_init = ud.CloudInit()
+            for content in rsrc.get('userdata', []):
+                cloud_init.add(content)
+
+            cloud_init.add_cloud_config({
+                'image_description': '',
+                'image_name': image,
+            })
+
+            tags = [{'ResourceType': 'instance',
+                     'Tags': [{'Key': 'Name',
+                               'Value': 'ImageBuild-{}'.format(image)}]}]
+
+            instance = ec2client.create_instance(
+                ec2_conn,
+                user_data=cloud_init.userdata(),
+                image_id=base_image_id,
+                instance_type='t2.small',
+                key=key,
+                tags=tags,
+                secgroup_ids=secgroup_id,
+                subnet_id=subnet_id,
+                instance_profile=instance_profile,
+                disk=10
+            )
+
+            _LOGGER.info(
+                'Started instance: %s', instance['Instances'][0]['InstanceId']
+            )
+
             return {}
 
-        def update(_rsrc_id, _rsrc):
-            """Update AWS image configuration."""
-            return {}
-
-        def delete(_rsrc_id):
+        def delete(rsrc_id):
             """Delete AWS image."""
-            pass
+            image = rsrc_id
+            _LOGGER.info('Create image: %s', image)
+
+            ec2_conn = awscontext.GLOBAL.ec2
+            sts_conn = awscontext.GLOBAL.sts
+            ec2client.delete_images(ec2_conn, name=image)
 
         self.list = _list
         self.get = get
         self.create = create
-        self.update = update
         self.delete = delete
 
 
--- lib/python/treadmill_aws/cli/aws/image.py
+++ lib/python/treadmill_aws/cli/aws/image.py
@@ -8,13 +8,16 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
+import gzip
+import io
 import logging
 
 import click
 
 from treadmill import cli
-from treadmill import context
 from treadmill import restclient
+
+from treadmill_aws import cli as aws_cli
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -35,7 +38,8 @@ def init():  # pylint: disable=R0912
     @click.group()
     @click.option('--api', help='API url to use.',
                   metavar='URL',
-                  envvar='TREADMILL_RESTAPI')
+                  type=cli.LIST,
+                  envvar='TREADMILL_AWSAPI')
     def image_group(api):
         """Manage Treadmill app monitor configuration"""
         ctx['api'] = api
@@ -45,7 +49,7 @@ def init():  # pylint: disable=R0912
     @_ON_EXCEPTIONS
     def configure(name):
         """Configure AWS image."""
-        restapi = context.GLOBAL.cell_api(ctx['api'])
+        restapi = ctx['api']
         url = _REST_PATH + name
         image_entry = restclient.get(restapi, url)
         cli.out(formatter(image_entry.json()))
@@ -54,9 +58,9 @@ def init():  # pylint: disable=R0912
     @_ON_EXCEPTIONS
     def _list():
         """List AWS images."""
-        restapi = context.GLOBAL.cell_api(ctx['api'])
+        restapi = ctx['api']
         url = _REST_PATH
-        response = restclient.get(restapi, url)
+        response = restclient.get(ctx['api'], url)
         cli.out(formatter(response.json()))
 
     @image_group.command()
@@ -64,12 +68,80 @@ def init():  # pylint: disable=R0912
     @_ON_EXCEPTIONS
     def delete(name):
         """Delete AWS image"""
-        restapi = context.GLOBAL.cell_api(ctx['api'])
+        restapi = ctx['api']
         url = _REST_PATH + name
         restclient.delete(restapi, url)
+
+    @image_group.command(name='create')
+    @click.option(
+        '--base-image',
+        required=True,
+        type=aws_cli.IMAGE,
+        help='Base image.'
+    )
+    @click.option(
+        '--base-image-account',
+        required=False,
+        help='Base image account.'
+    )
+    @click.option(
+        '--userdata',
+        required=True,
+        type=click.Path(exists=True),
+        multiple=True,
+        help='Cloud-init user data.'
+    )
+    @click.option(
+        '--instance-profile',
+        required=False,
+        help='Instance profile with create image privs.'
+    )
+    @click.option(
+        '--secgroup',
+        required=False,
+        type=aws_cli.SECGROUP,
+        help='Security group'
+    )
+    @click.option(
+        '--subnet',
+        required=False,
+        type=aws_cli.SUBNET,
+        help='Subnet'
+    )
+    @click.option(
+        '--key',
+        help='SSH key'
+    )
+    @click.argument('name', required=True, type=str)
+    @_ON_EXCEPTIONS
+    def create(base_image, base_image_account, userdata, instance_profile,
+               secgroup, subnet, key, name):
+        """Create image"""
+        restapi = ctx['api']
+        payload = {
+            'base_image': base_image,
+            'base_image_account': base_image_account,
+            'instance_profile': instance_profile,
+            'userdata': [],
+            'secgroup': secgroup,
+            'subnet': subnet,
+            'key': key,
+        }
+
+        for filename in userdata:
+            with io.open(filename, 'rb') as f:
+                content = f.read()
+                if filename.endswith('.gz'):
+                    content = gzip.decompress(content)
+                payload['userdata'].append(content.decode())
+
+        url = _REST_PATH + name
+        response = restclient.post(restapi, url, payload=payload)
+        print(response.json())
 
     del delete
     del _list
     del configure
+    del create
 
     return image_group
